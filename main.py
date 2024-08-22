68be8b0715b9d5a77d65225d2c21f03b756537af
Ilemona <ilemona02+git@gmail.com>
2024-08-22 12:18:49 -0600

    sprite class for animation

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     
+    server_clock = pygame.time.Clock()
+    dt = 0
+    
     running = True
     while running:
         for event in pygame.event.get():
@@ -15,7 +18,7 @@ def main():
         
         screen.fill((0, 0, 0))  # Fill the screen with black color
         pygame.display.flip()   # Refresh the screen
-    
+        dt = server_clock.tick(60) / 1000
     pygame.quit()
 
 if __name__ == "__main__":
