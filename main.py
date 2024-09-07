60902acce4faddcc056114d1cd76120f78d2787b
Ilemona <ilemona02+git@gmail.com>
2024-09-06 19:47:47 -0600

    group container to manage objects

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
     
-    server_clock = pygame.time.Clock()
+    # Create the groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # Assign the groups to the Player class
+    Player.containers = (updatable, drawable)
+    
+    # Create the player object after setting the containers
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
-    running = True
-    while running:
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
-        screen.fill((0, 0, 0))  # Fill the screen with black color
-        pygame.display.flip()   # Refresh the screen
-        dt = server_clock.tick(60) / 1000
-    pygame.quit()
+
+        screen.fill("black")
+        
+        # Draw all drawable objects
+        for entity in drawable:
+            entity.draw(screen)
+        
+        pygame.display.flip()
+        
+        # Update all updatable objects
+        for entity in updatable:
+            entity.update(dt)
+        
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
 
 class Player(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
-        self.position = pygame.Vector2(x, y)
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        #self.position = pygame.Vector2(x, y)
         self.rotation = 0
 
     def triangle(self):
@@ -14,4 +15,29 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def draw(self, screen):
+        points = self.triangle()
+        color = (255, 255, 255)  # White color
+        line_width = 2
+        pygame.draw.polygon(screen,"white", points, line_width)
+        
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_s]:
+            self.rotate(-dt) 
+        if keys[pygame.K_w]:
+            self.rotate(dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+            
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
         
