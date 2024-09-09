6ae7e5bd9daf241aa2666c4bac04687ca680a402
Ilemona <ilemona02+git@gmail.com>
2024-09-09 17:21:57 -0600

    asteroid end

--- main.py
+++ main.py
@@ -60,15 +60,11 @@ def main():
                 return
             for s in shots:
                 if a.check_collision(s):
-                    a.kill()
                     s.kill()
-        
-        
-                
+                    a.split()        
         
         # Limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
-    
     main()
